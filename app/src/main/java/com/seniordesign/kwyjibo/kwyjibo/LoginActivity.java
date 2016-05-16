eb7c1135c2cdd4e8a9b62dbab81c1bf5bd438531
Chris <Chris2Schill@gmail.com>
2016-05-16 14:35:02 -0400

    rename test

--- app/src/main/java/com/seniordesign/kwyjibo/kwyjibo/LoginActivity.java
+++ app/src/main/java/com/seniordesign/kwyjibo/kwyjibo/LoginActivity.java
@@ -43,7 +43,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
     /**
      * Id to identity READ_CONTACTS permission request.
      */
-    private static final int REQUEST_READ_CONTACTS = 0;
+    private static final int READCONTACTS = 0;
 
     /**
      * A dummy authentication store containing known user names and passwords.
@@ -116,11 +116,11 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
                         @Override
                         @TargetApi(Build.VERSION_CODES.M)
                         public void onClick(View v) {
-                            requestPermissions(new String[]{READ_CONTACTS}, REQUEST_READ_CONTACTS);
+                            requestPermissions(new String[]{READ_CONTACTS}, READCONTACTS);
                         }
                     });
         } else {
-            requestPermissions(new String[]{READ_CONTACTS}, REQUEST_READ_CONTACTS);
+            requestPermissions(new String[]{READ_CONTACTS}, READCONTACTS);
         }
         return false;
     }
@@ -131,7 +131,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
                                            @NonNull int[] grantResults) {
-        if (requestCode == REQUEST_READ_CONTACTS) {
+        if (requestCode == READCONTACTS) {
             if (grantResults.length == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 populateAutoComplete();
             }
