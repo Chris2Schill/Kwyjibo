1eac106650cfbe7717924d84254b54150b6e32ed
Chris <Chris2Schill@gmail.com>
2016-05-16 18:56:27 -0400

    Stored the guest button reference to the class's field. Added Comments to explain what's going on

--- app/src/main/java/com/seniordesign/kwyjibo/kwyjibo/LoginFragment.java
+++ app/src/main/java/com/seniordesign/kwyjibo/kwyjibo/LoginFragment.java
@@ -12,10 +12,18 @@ import android.widget.Button;
 public class LoginFragment extends Fragment {
     private Button guestLoginButton;
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // In android, all ui-elements extend the 'View' class.
+        // rootView is the RelativeLayout from login_fragment.xml
         View rootView = inflater.inflate(R.layout.login_fragment, container, false);
 
 
-        rootView.findViewById(R.id.guest_login_button).setOnClickListener(new View.OnClickListener(){
+        // This retrieves the ui-element reference pointers from the rootViews view hierarchy.
+        guestLoginButton = (Button)rootView.findViewById(R.id.guest_login_button);
+
+        // This assigns the View.OnClickListener interface to this button.
+        // This syntax is weird. All that is happening here is overriding
+        // the onClick method of the instantiated object on the fly.
+        guestLoginButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
                 FragmentTransaction transaction = getFragmentManager().beginTransaction();
