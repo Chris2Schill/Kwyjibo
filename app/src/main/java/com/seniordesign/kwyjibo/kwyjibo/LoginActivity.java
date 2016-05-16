6616842b63d9b0ab7ea320fa4f74a753a5e0dfd1
Eric Wysocki <Eric Wysocki>
2016-05-16 14:21:45 -0400

    ericedit commit test

--- app/src/main/java/com/seniordesign/kwyjibo/kwyjibo/LoginActivity.java
+++ app/src/main/java/com/seniordesign/kwyjibo/kwyjibo/LoginActivity.java
@@ -33,7 +33,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static android.Manifest.permission.READ_CONTACTS;
-
+//Eric's Test Comment from 'ericedit'
 
 /**
  * A login screen that offers login via email/password.
