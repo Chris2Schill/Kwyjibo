702a7db8620678e9358a4e5c5850ff90cc1e6c11
Chris <Chris2Schill@gmail.com>
2016-07-03 15:37:44 -0400

    Added logic for swapping between login and signup views. Also animations.

--- app/src/main/java/com/seniordesign/kwyjibo/fragments/login_signup/IntroTitleFragment.java
+++ app/src/main/java/com/seniordesign/kwyjibo/fragments/login_signup/IntroTitleFragment.java
@@ -1,12 +1,23 @@
 package com.seniordesign.kwyjibo.fragments.login_signup;
 
+import android.animation.AnimatorInflater;
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
 import android.os.Bundle;
+import android.support.graphics.drawable.AnimatedVectorDrawableCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.LayoutInflaterCompat;
+import android.util.AttributeSet;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
+import com.seniordesign.kwyjibo.FlipListener;
 import com.seniordesign.kwyjibo.activities.MainActivity;
 import com.seniordesign.kwyjibo.kwyjibo.R;
 
@@ -18,39 +29,56 @@ public class IntroTitleFragment extends Fragment {
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.intro_title_fragment, container, false);
-        setOnClickListeners(rootView);
         MainActivity.applyLayoutDesign(rootView);
+        setOnClickListeners(rootView);
         return rootView;
     }
 
     private void setOnClickListeners(View v){
+
         v.findViewById(R.id.login_button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Fragment loginFragment = MainActivity.getFragment(MainActivity.Screens.LOGIN);
+                Fragment signupFragment = MainActivity.getFragment(MainActivity.Screens.SIGNUP);
+
                 FragmentTransaction t = getActivity().getSupportFragmentManager().beginTransaction();
                 t.setCustomAnimations(R.anim.slide_in_top, R.anim.slide_out_top,
-                                      R.anim.slide_in_top, R.anim.slide_out_top);
-                t.add(R.id.main_activity_fragment_container,
-                        MainActivity.getFragment(MainActivity.Screens.LOGIN));
-                t.addToBackStack("LOGIN");
+                        R.anim.slide_in_top, R.anim.slide_out_top);
+
+                if (!loginFragment.isAdded()){
+                    t.add(R.id.main_activity_fragment_container,loginFragment);
+                    t.addToBackStack("LOGIN");
+                }
+                if (signupFragment.isAdded()){
+                    getActivity().getSupportFragmentManager().popBackStack();
+                    t.hide(signupFragment);
+                }
+
+                t.show(loginFragment);
                 t.commit();
-//                MainActivity.replaceScreen(MainActivity.Screens.LOGIN, "LOGIN",
-//                        R.anim.slide_in_top, R.anim.slide_out_top);
             }
         });
 
-        v.findViewById(R.id.intro_fragment_signup_button) .setOnClickListener(new View.OnClickListener() {
+        v.findViewById(R.id.intro_fragment_signup_button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Fragment signupFragment = MainActivity.getFragment(MainActivity.Screens.SIGNUP);
+                Fragment loginFragment = MainActivity.getFragment(MainActivity.Screens.LOGIN);
+
                 FragmentTransaction t = getActivity().getSupportFragmentManager().beginTransaction();
                 t.setCustomAnimations(R.anim.slide_in_top, R.anim.slide_out_top,
-                                      R.anim.slide_in_top, R.anim.slide_out_top);
-                t.add(R.id.main_activity_fragment_container,
-                        MainActivity.getFragment(MainActivity.Screens.SIGNUP));
-                t.addToBackStack("SIGNUP");
+                        R.anim.slide_in_top, R.anim.slide_out_top);
+                if (!signupFragment.isAdded()) {
+                    t.add(R.id.main_activity_fragment_container, signupFragment);
+                    t.addToBackStack("SIGNUP");
+                }
+                if (loginFragment.isAdded()){
+                    getActivity().getSupportFragmentManager().popBackStack();
+                    t.hide(loginFragment);
+                }
+                t.show(signupFragment);
                 t.commit();
-//                MainActivity.replaceScreen(MainActivity.Screens.SIGNUP, "SIGNUP",
-//                        R.anim.slide_in_top, R.anim.slide_out_top);
             }
         });
     }
