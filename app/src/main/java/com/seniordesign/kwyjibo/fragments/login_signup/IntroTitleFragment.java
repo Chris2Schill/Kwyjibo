bb4242d838df2af21f10d622d97eed5754894609
Chris <Chris2Schill@gmail.com>
2016-07-04 00:20:41 -0400

    Added back necessary import.

--- app/src/main/java/com/seniordesign/kwyjibo/fragments/login_signup/IntroTitleFragment.java
+++ app/src/main/java/com/seniordesign/kwyjibo/fragments/login_signup/IntroTitleFragment.java
@@ -1,6 +1,7 @@
 package com.seniordesign.kwyjibo.fragments.login_signup;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
